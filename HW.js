76007aa5fed10c33e76ba830fc61cf4f710bcadd
Klim D. <klimdudko@outlook.com>
2024-03-28 13:41:20 +0100

    1/10

--- HW.js
+++ HW.js
@@ -1,1 +1,13 @@
 "use strict";
+
+//1-st task
+const stringTrue = "true";
+const isTheSkyIsGreen = false;
+const foreverYoung = 17;
+let missingData ;
+let user = null; 
+
+const result = [stringTrue + isTheSkyIsGreen + foreverYoung + missingData + user];
+
+result.forEach((result) => console.log(typeof result)); 
+
