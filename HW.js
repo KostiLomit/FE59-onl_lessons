3406408668ce6fdce33b5aae738dd00bcbb50777
Klim D. <klimdudko@outlook.com>
2024-03-30 11:50:57 +0100

    test branch commit

---
+++ HW.js
@@ -0,0 +1,156 @@
+"use strict";
+
+//1-st task
+const stringTrue = "true";
+const isTheSkyIsGreen = false;
+const foreverYoung = 17;
+let missingData ;
+let user = null; //typeoff null возвращает object
+
+const result = [stringTrue , isTheSkyIsGreen , foreverYoung , missingData , user]; //создали масситв который содержит наши переменные 
+
+result.forEach((result) => console.log(typeof result)); //метод .forEach перебирает массив, и для кажжого элемента выводим его тип используя .typeof result
+
+//2-nd task 
+const height = 15;
+const width = 20; 
+
+if (height < width) {
+    console.log(width);
+} else if (width > height) {
+    console.log(height);
+} else {
+    console.log("значения равны");
+}
+
+//3-rd task
+for (let i = 1; i <= 20; ++i) {
+    if (i%3==0)
+    console.log(i);
+}
+
+//4-task
+    const key = true;
+    const documents = true; 
+    const pen = true; 
+    const apple = false; 
+    const orange = true; 
+
+    const shouldGoToWork = key && documents && pen && (apple || orange);
+
+    console.log(shouldGoToWork);// Мы можем идти на работу :(
+
+//5-task
+    let yourNumber = prompt("введите Целое число: "); // просим ввести число
+
+    let number = parseInt(yourNumber); //конвертируем значение в число для проверочек
+
+    // хотелось бы проверку на целое число и на пустое поле тоже :D
+    
+    if (!isNaN (number) && yourNumber.trim() !== "") {
+
+        if (number%3 == 0 && number%5 ==0){ 
+            alert ("FizBuz");
+            
+        }
+         else if (number%3 == 0) {
+            alert ("Fiz");
+        }
+         else if (number%5 == 0) {
+            alert ("Buz");
+        }
+    } 
+     else {
+        alert ("Давай серьезно, только целое число !");
+    }
+
+//6-task
+    let yourAge = prompt("Ваш возраст: " + "\nтолько целые числа\n"); // перенос строки в promt
+
+    let numberAge = parseInt(yourAge);
+
+    if (!isNaN(numberAge) && yourAge.trim() !== "") {
+
+        if (numberAge > 16 && numberAge <18) {
+            alert ("Можешь выкурить сигаретку, только маме не говори");
+        }
+        
+        else if (numberAge >= 18) {
+            alert ("Попей пивка");
+        }
+
+        else if (numberAge < 18) {
+            alert ("Пей колу");
+        }
+    }
+
+    else {
+        alert ("Давай серьезно, только целое число !");
+     }
+
+//7-task
+let userCauntry = prompt("Куда бы вы хотели отправится ?\nВарианты:\n\nЮг\nСевер\nЗапад\nВосток\n ");
+
+switch (userCauntry) {
+    case "Юг":
+        alert ("на юг пойдешь счастье найдешь");
+    break;
+    case "Север":
+       alert ("на север пойдешь много денег найдешь");
+    break; 
+    case "Запад":
+        alert ("на запад пойдешь верного друга найдешь");
+    break;
+    case "Восток":
+        alert ("на восток пойдешь разработчиком станешь");
+    break;
+    default: 
+        alert ("туда не едут поезда и не идут посылки");
+}
+
+//LEVEL - UP
+
+let originalUser = "пОлИнА нАбЕрЕжНаЯ"; 
+
+function capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(); //функция которая изменяет строку таким образом что бы первая буква сттала заглавной, а остальные строчными
+}
+
+let changedUser = originalUser.toLowerCase().split(" ").map(capitalize).join(" "); //
+
+
+// let veryChangedUser = changedUser.split(" "); 
+
+alert(`Привет, ${changedUser}!`);
+
+//9-task
+
+let userNumber = prompt("Введите число");
+
+let userAction1 = prompt("Сколько отнять?");
+let userAction2 = prompt("Сколько прибавить?");
+let userAction3 = prompt("Сколько умножить?");
+let userAction4 = prompt("Сколько разделить?");
+
+// Преобразование строк в числа
+userNumber = parseInt(userNumber);
+let subtraction = parseInt(userAction1);
+let addition = parseInt(userAction2);
+let multiplication = parseInt(userAction3);
+let division = parseInt(userAction4);
+
+
+const result = ((userNumber - subtraction) + addition) * multiplication / division;
+const formula = `((${userNumber} - ${subtraction}) + ${addition}) * ${multiplication} / ${division} = ${result}`;
+
+
+alert(formula);
+
+//10-task
+
+let object = ""; 
+
+for (let i = 0; i < 6; i++) { 
+    object += "#"; 
+    console.log(object); 
+}
